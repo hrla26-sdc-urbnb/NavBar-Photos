22374aeaa96d5577c088468fa4d53589e7ec711e
Tommy Liao <tommyliao340@hotmail.com>
2019-01-03 00:24:07 -0800

    Slight css module tweaks, back to where I began before refactor

--- react-client/src/components/mainnavbar/modals/SignupWithEmail.jsx
+++ react-client/src/components/mainnavbar/modals/SignupWithEmail.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from '../../../styles/signlogmodals.css';
 
 const SignupWithEmail = () => (
- <div className='modalcontainer'>
-  <div className='innermodal emailsu'>
+ <div className={s.modalcontainer}>
+  <div className={s.innermodal}>
     signup with email
   </div>
  </div>
