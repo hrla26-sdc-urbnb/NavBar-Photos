24f048df879fdf274620465ad13666a812614d4a
Tommy Liao <tommyliao340@hotmail.com>
2019-01-03 11:35:58 -0800

    Changed font referencing, no longer clashes with other items

--- react-client/src/components/MainNavBarPhotos.jsx
+++ react-client/src/components/MainNavBarPhotos.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MainNavBar from './mainnavbar/MainNavBar.jsx';
 import SecondaryNavBar from './SecondaryNavBar.jsx';
 import PhotoDisplay from './PhotoDisplay.jsx';
+import s from '../styles/mainnavbar.css';
 
 export default class MainNavBarPhotos extends React.Component {
     constructor(props) {
@@ -14,7 +15,7 @@ export default class MainNavBarPhotos extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className={s.navbarphotos}>
                 <MainNavBar />
                 {this.state.showSecondary && <div className="secondarynavbar"><SecondaryNavBar /></div>}
                 <div className="photodisplay"><PhotoDisplay /></div>
