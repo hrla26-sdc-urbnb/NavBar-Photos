2cfdd1e78f08a5d7dbe5faeb67c7f2761804400d
Tommy Liao <tommyliao340@hotmail.com>
2019-01-03 00:02:03 -0800

    Responsive photo display added

--- react-client/src/components/PhotoDisplay.jsx
+++ react-client/src/components/PhotoDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import s from '../styles/photodisplay.css';
 import PhotoDisplayModal from './PhotoDisplayModal.jsx';
 
 export default class PhotoDisplay extends React.Component {
@@ -18,7 +19,7 @@ export default class PhotoDisplay extends React.Component {
 
     render() {
         return (
-            <div className='photodisplaywrapper'>
+            <div className={s.photodisplaywrapper}>
                 photo display!!
             </div>
         )
