9d1b97f9ec5a77893f9d0bd7380d34f61cbab78a
Tommy Liao <tommyliao340@hotmail.com>
2019-01-06 18:48:53 -0800

    Photo data seeded, actual photos should be up

--- react-client/src/components/PhotoDisplay.jsx
+++ react-client/src/components/PhotoDisplay.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
 import axios from 'axios';
+import classNames from 'classnames';
 import s from '../styles/photodisplay.css';
 import PhotoDisplayModal from './PhotoDisplayModal.jsx';
 
 export default class PhotoDisplay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            idxPhotoHovered: null,
-            photoHovered: false,
-            photos: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      idxPhotoHovered: null,
+      photoHovered: false,
+      photos: []
     }
+    this.getPhotos = this.getPhotos.bind(this);
+  }
 
-    getPhotos() {
-        axios.get('api/photos/')    
-    }
+  componentDidMount() {
+    this.getPhotos();
+    document.addEventListener('click', (e) => console.log(e.target.id));
+  }
 
-    render() {
-        return (
-            <div className={s.photodisplaywrapper}>
-                photo display!!
-            </div>
-        )
-    }
+  getPhotos() {
+    const randNum = Math.floor(Math.random() * 100);
+    axios.get(`api/photos/${randNum}`)
+    .then(p => {
+      const photos = p.data.map(obj => obj.url);
+      this.setState({ photos });
+    });
+  }
+
+  render() {
+    return (
+      <div className={s.photodisplaywrapper}>
+        <div className={s.bigphotocontainer}>
+          <img id='1' className={s.bigphoto} src={`${this.state.photos[0]}`}></img>
+        </div>
+        <div className={s.smallphotocontainer}>
+          <img id='2' className={s.smallphoto} src={`${this.state.photos[1]}`}></img>
+          <img id='3' className={classNames(s.smallphoto, s.row2)} src={`${this.state.photos[2]}`}></img>
+          <img id='4' className={s.smallphoto} src={`${this.state.photos[3]}`}></img>
+          <img id='5' className={classNames(s.smallphoto, s.row2)} src={`${this.state.photos[4]}`}></img>
+        </div>
+      </div>
+    )
+  }
 }
--- rest-server/app.js
+++ rest-server/app.js
@@ -7,9 +7,15 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../react-client/public')));
 
 app.get('/api/photos/:id', (req, res) => {
-  Photos.findAll({ where: { listing_id: req.params.id } })
-    .then(data => res.status(200).send(data))
-    .catch(err => console.log(err));
+  if (req.params.id < 101) {
+    Photos.findAll({ attributes: ['url'], where: { listing_id: req.params.id }, limit: 5 })
+      .then(data => res.status(200).send(data))
+      .catch(err => console.log(err));
+  } else {
+    Photos.findAll({ where: { listing_id: (req.params.id - 100) } })
+      .then(data => res.status(200).send(data))
+      .catch(err => console.log(err));
+  }
 });
 
 module.exports = app;
